afad9963edda2e633c268c8eb2092569de438710
Joao <joaovic2012@hotmail.com>
2022-11-27 22:20:07 -0300

    GET Clients paged

--- src/main/java/com/devsuperior/crud/controllers/ClientController.java
+++ src/main/java/com/devsuperior/crud/controllers/ClientController.java
@@ -3,6 +3,8 @@ package com.devsuperior.crud.controllers;
 import com.devsuperior.crud.dto.ClientDTO;
 import com.devsuperior.crud.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +20,12 @@ public class ClientController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
         ClientDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        Page<ClientDTO> dto = service.findAll(pageable);
         return ResponseEntity.ok(dto);
     }
 }
