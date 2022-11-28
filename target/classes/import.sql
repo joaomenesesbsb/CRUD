5411b3c449ca596cff556f250d89290b15ed7121
Joao <joaovic2012@hotmail.com>
2022-11-27 21:42:25 -0300

    H2 test

---
+++ target/classes/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('João Victor Meneses', '020.060.321-20', 2000.00, '1998-07-02',1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Fabricio Silva', '085.985.032-60', 1500.00, '1996-04-15',null);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Estela Lima', '055.466.365-98', 1200.00, '2000-01-26',null);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Maria Teresa Tavares', '104.255.480-45', 4200.00, '1987-09-03',2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Selton Abreu', '040.142.321-21', 2520.00, '1998-12-07',3);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Rafael Medeiros', '061.325.462-30', 2000.00, '1999-07-22',1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Julia Silva', '033.268.887-54', 6000.00, '1992-09-02',2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Bianca Araujo', '022.235.731-65', 1500.00, '1997-05-10',null);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Carlos Ferreira', '041.254.842-30', 1400.00, '2001-02-12',1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Weverton Guimarães', '031.205.34-75', 2100.00, '1998-08-25',4);
+
