c2dc72736a619526b2c8c95dcb214eee69a2da33
Joao <joaovic2012@hotmail.com>
2022-11-27 22:14:18 -0300

    GET client by id

--- src/main/java/com/devsuperior/crud/services/ClientService.java
+++ src/main/java/com/devsuperior/crud/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.crud.services;
 
+import com.devsuperior.crud.dto.ClientDTO;
+import com.devsuperior.crud.entities.Client;
 import com.devsuperior.crud.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,4 +13,9 @@ public class ClientService {
     @Autowired
     private ClientRepository repository;
 
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Client client = repository.findById(id).get();
+        return new ClientDTO(client);
+    }
 }
