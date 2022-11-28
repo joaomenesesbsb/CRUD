06bb15d9e192b4b91fb17f3347c9cd1d019c06a2
Joao <joaovic2012@hotmail.com>
2022-11-27 22:04:42 -0300

    ClientDTO and Clientrepository

---
+++ src/main/java/com/devsuperior/crud/dto/ClientDTO.java
@@ -0,0 +1,68 @@
+package com.devsuperior.crud.dto;
+
+import com.devsuperior.crud.entities.Client;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+public class ClientDTO {
+
+    private Long id;
+    @Size(min = 3, message = "Descrição precisa ter no minimo 3 caracteres")
+    @NotBlank(message = "Campo requerido")
+    private String name;
+    @Size(min = 10, message = "Descrição precisa ter no minimo 14 caracteres")
+    @NotBlank(message = "Campo requerido")
+    private String cpf;
+    @Positive(message = "O preço deve ser positivo")
+    private Double income;
+    @PastOrPresent
+    private LocalDate birthDate;
+    private Integer children;
+
+    public ClientDTO(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public ClientDTO(Client entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.cpf = entity.getCpf();
+        this.income = entity.getIncome();
+        this.birthDate = entity.getBirthDate();
+        this.children = entity.getChildren();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+}
---
+++ src/main/java/com/devsuperior/crud/services/ClientService.java
@@ -0,0 +1,14 @@
+package com.devsuperior.crud.services;
+
+import com.devsuperior.crud.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository repository;
+
+}
